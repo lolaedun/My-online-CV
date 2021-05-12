43f1c2370e471a39216657a11cf79240ce8a4b10
Lola <mledun@hotmail.com>
2021-05-12 19:29:52 +0000

    added alert to email request

--- assets/js/sendMail.js
+++ assets/js/sendMail.js
@@ -7,12 +7,37 @@ function sendMail(contactForm) {
     .then(
         function(response) {
             console.log("SUCCESS", response.status, response.text);
+            alert('Your mail is sent!');
         },
         function(error) {
             console.log("FAILED", error);
+            alert('Oops... ' + JSON.stringify(error));
         }
     );
     return false;  // To block from loading a new page
 }
 
 
+/* code fragment
+var data = {
+    service_id: "service_py25tol",
+    template_id: "template_l8uiwzc",
+    user_id: "user_ycoJhCrC9L3ODSSESOoKj",
+    template_params: {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+        
+    }
+};
+ 
+$.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+    type: 'POST',
+    data: JSON.stringify(sendMail),
+    contentType: 'application/json'
+}).done(function() {
+    alert('Your mail is sent!');
+}).fail(function(error) {
+    alert('Oops... ' + JSON.stringify(error));
+});
+*/
