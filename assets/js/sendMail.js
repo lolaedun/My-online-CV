c231b7b9667d55871eb8a95a3ab8313da29b38fe
Lola <mledun@hotmail.com>
2021-05-12 19:09:58 +0000

    added emailjs integration on contact page

--- assets/js/sendMail.js
+++ assets/js/sendMail.js
@@ -1,1 +1,18 @@
-console.log('hello');
+function sendMail(contactForm) {
+    emailjs.send("service_py25tol","template_l8uiwzc", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response.status, response.text);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
+
+
